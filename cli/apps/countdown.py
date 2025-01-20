6d8c7b27e0084641824b96f04fba3907920e98c5
Raphael Loeffler <raphael.loeffler2010@gmail.com>
2025-01-20 15:44:03 +0100

    added files

--- cli/apps/countdown.py
+++ cli/apps/countdown.py
@@ -1,4 +1,17 @@
-import time
+from time import sleep
 
 def countdown(t: int) -> None:
-    ...
+    if t >= 0:
+        while t:
+            mins, secs = divmod(t, 60)
+            time_elapsed: str = f"{mins:02d}:{secs:02d}"
+            print(time_elapsed, end='\r')
+            sleep(1)
+            t -= 1
+        print("Time is up!")
+    else:
+        print("No negative numbers are accepted")
+
+if __name__ == "__main__":
+    t = int(input("Time (seconds): "))
+    countdown(t)
