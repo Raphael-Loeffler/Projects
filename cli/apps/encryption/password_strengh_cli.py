1f503ad5e31dfb7d7206a0de26387cf7608eb006
Raphael Loeffler <raphael.loeffler2010@gmail.com>
2025-01-21 17:35:11 +0100

    added files

---
+++ cli/apps/encryption/password_strengh_cli.py
@@ -0,0 +1,7 @@
+"""
+What is a strong password:
+-at least 6 characters... -> read about it
+-Letters (a - A)
+-Numbers(0-9)
+-does not repeat characters
+"""
---
+++ cli/apps/encryption/random_password_generator_cli.py
@@ -0,0 +1,21 @@
+from random import shuffle, choice
+from string import ascii_letters, digits, punctuation
+
+
+def generate_password(length: int) -> str:
+  characters: list = list(ascii_letters + digits + punctuation)
+  shuffle(characters)
+  password_list: list = [choice(characters) for _ in range(length)]
+  return "".join(password_list)
+
+def run() -> str:
+  length: int = int(input("Password length: "))
+  password: str = ""
+  if length > 0:
+    return generate_password(length)
+  return "Only positive password length is accepted"
+
+
+if __name__ == "__main__":
+  password = run()
+  print(f"{password=}")
