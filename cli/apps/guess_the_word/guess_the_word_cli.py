d23d1eaaf0d38d92545a45531a61bdd42552ef8a
Raphael Loeffler <raphael.loeffler2010@gmail.com>
2025-02-05 22:03:24 +0100

    added files

--- cli/apps/guess_the_word/guess_the_word_cli.py
+++ cli/apps/guess_the_word/guess_the_word_cli.py
@@ -1,17 +1,33 @@
 from random import choice
 from string import ascii_uppercase
 
-# TODO: Expand words list...
-# TODO: Debug line 25 - 35
-
-# TODO: in webbrowser_cli.py
-# TODO: EXERCISES:
-# TODO: 1. Get an input
-# TODO: 2. Create an -html file. For example for input 'www.google.com' -> google.html
 
 
 LIVES: int = 5
-WORDS: list[str] = ["Simon", "Raphael", "Michalis", "Tina"]
+WORDS = [
+    "apple", "banana", "orange", "grape", "cherry", "mango", "peach", "lemon", "pear", "plum",
+    "table", "chair", "couch", "shelf", "desk", "lamp", "mirror", "window", "door", "carpet",
+    "house", "apartment", "cabin", "castle", "garage", "garden", "kitchen", "bedroom", "bathroom", "roof",
+    "dog", "cat", "rabbit", "horse", "elephant", "lion", "tiger", "monkey", "giraffe", "zebra",
+    "ocean", "river", "lake", "pond", "waterfall", "stream", "beach", "island", "shore", "harbor",
+    "cloud", "rain", "storm", "thunder", "lightning", "snow", "wind", "hurricane", "tornado", "fog",
+    "music", "guitar", "piano", "violin", "trumpet", "drums", "flute", "saxophone", "song", "melody",
+    "car", "bus", "bicycle", "motorcycle", "train", "airplane", "helicopter", "boat", "ship", "submarine",
+    "computer", "keyboard", "mouse", "screen", "laptop", "printer", "speaker", "microphone", "camera", "phone",
+    "book", "magazine", "newspaper", "novel", "dictionary", "journal", "poem", "story", "letter", "document",
+    "school", "teacher", "student", "classroom", "homework", "exam", "library", "lesson", "notebook", "pencil",
+    "fire", "earth", "water", "air", "metal", "wood", "stone", "sand", "glass", "plastic",
+    "happiness", "sadness", "anger", "fear", "love", "hate", "surprise", "curiosity", "pride", "guilt",
+    "day", "night", "morning", "afternoon", "evening", "sunrise", "sunset", "midnight", "dawn", "dusk",
+    "city", "village", "town", "country", "capital", "suburb", "street", "avenue", "square", "market",
+    "doctor", "nurse", "police", "firefighter", "teacher", "engineer", "scientist", "chef", "pilot", "artist",
+    "bread", "butter", "cheese", "egg", "meat", "fish", "vegetable", "fruit", "soup", "salad",
+    "smile", "laugh", "cry", "yawn", "wink", "hug", "kiss", "wave", "shout", "whisper",
+    "fast", "slow", "big", "small", "tall", "short", "hot", "cold", "bright", "dark",
+    "jump", "run", "walk", "sit", "stand", "dance", "swim", "climb", "fly", "crawl",
+    "circle", "square", "triangle", "rectangle", "oval", "cube", "sphere", "pyramid", "diamond", "star",
+    "simon", "raphael", "michalis", "tina", "dog", "cat", "codemanufaktur", "python", "java"
+]
 
 def get_word(words: list[str]) -> str:
   word: str = choice(words)
@@ -19,8 +35,8 @@ def get_word(words: list[str]) -> str:
 
 def play() -> None:
   print("<explanation>")
-  word: str = get_word(WORDS)
-  letters_word: set[str] = set(word)
+  word: str = get_word(WORDS).upper()
+  letters_word: set[str] = set(word.upper())
   abc: set[str] = set(ascii_uppercase)
   used_letters: set[str] = set()
   lives: int = LIVES
@@ -29,7 +45,7 @@ def play() -> None:
     print(f"Lives: {lives}")
     print(f"Letters used: {''.join(used_letters)}")
     word_list_print: list[str] = [letter if letter in used_letters else '_' for letter in word]
-    print(f"Word {' '.join(word_list_print)}\n")
+    print(f"Word: {' '.join(word_list_print)}\n")
     user_letter: str = input("Guess a letter: ").upper()
     if user_letter in abc - used_letters:
       used_letters.add(user_letter)
@@ -43,8 +59,9 @@ def play() -> None:
       print("Already used...")
     else:
       print("Invalid character. Try again!")
-    print("".join('_ ' for _ in word)+"\n")
   
+  print(f"Letters used: {''.join(used_letters)}")
+  print(f"Word: {word}")
   if lives == 0:
       print("LOST")
   else:
--- cli/apps/webbrowser/code/webbrowser_cli.py
+++ cli/apps/webbrowser/code/webbrowser_cli.py
@@ -1,11 +1,17 @@
 from socket import socket, AF_INET, SOCK_STREAM, timeout
 
+# TODO: in webbrowser_cli.py
+# TODO: EXERCISES:
+# TODO: 1. Get an input
+# TODO: 2. Create an -html file. For example for input 'www.google.com' -> google.html
 
+# TODO! Delete data at the beginning
 
 PORT: int = 80
 BYTE_BUFFER_SIZE: int = 4096
 
-def get_url(url: str) -> str:
+def get_url(url: str, filename: str) -> None:
+   print("")
    client_socket: socket = socket(AF_INET, SOCK_STREAM) # AF_INET -> IPv4 & IPv6, SOCK_STREAM -> TCP
    client_socket.settimeout(5) # DoS -> Decline of Service
    client_socket.connect((url, PORT)) # HTTP: 80, HTTPS: 443
@@ -21,10 +27,13 @@ def get_url(url: str) -> str:
       except timeout:
          break
    client_socket.close()
-   return response.decode('latin-1')
+
+   with open(f"/home/raphael/GitHub/Projects/cli/apps/webbrowser/files/{filename}.html", 'w') as file:
+      file.write(response.decode('latin-1'))
+
 
 
 if __name__ == "__main__":
-   url: str = "www.codemanufaktur.com"
-   response: str = get_url(url)
-   print(f"{response}")
+   url: str = input("Please enter url: ")
+   name: str = input("Please enter filename: ")
+   get_url(url=url, filename=name)
