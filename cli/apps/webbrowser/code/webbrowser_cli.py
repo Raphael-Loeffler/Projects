31fb6204cf45079dd3d0ee23678d9002f8e870a7
Raphael Loeffler <raphael.loeffler2010@gmail.com>
2025-02-08 18:17:10 +0100

    added feature in webbrowser_cli.py

--- cli/apps/webbrowser/code/webbrowser_cli.py
+++ cli/apps/webbrowser/code/webbrowser_cli.py
@@ -2,10 +2,9 @@ from socket import socket, AF_INET, SOCK_STREAM, timeout
 
 # TODO: in webbrowser_cli.py
 # TODO: EXERCISES:
-# TODO: 1. Get an input
-# TODO: 2. Create an -html file. For example for input 'www.google.com' -> google.html
-
-# TODO! Delete data at the beginning
+# TODO: 1. Get an input - Done 
+# TODO: 2. Create an -html file. For example for input 'www.google.com' -> google.html - Done
+# TODO Delete data at the beginning - Done
 
 PORT: int = 80
 BYTE_BUFFER_SIZE: int = 4096
@@ -27,9 +26,13 @@ def get_url(url: str, filename: str) -> None:
       except timeout:
          break
    client_socket.close()
-
-   with open(f"/home/raphael/GitHub/Projects/cli/apps/webbrowser/files/{filename}.html", 'w') as file:
-      file.write(response.decode('latin-1'))
+   
+   with_html_ending: str = f"/home/raphael/GitHub/Projects/cli/apps/webbrowser/files/{filename}.html"
+   without_html_ending: str = f"/home/raphael/GitHub/Projects/cli/apps/webbrowser/files/{filename}"
+   
+   with open(with_html_ending if filename[len(filename) - 5:] != ".html" else without_html_ending, 'w') as file:
+      start_of_html_code_index: int = response.decode('latin-1').find("<!doctype html>")
+      file.write(response.decode('latin-1')[start_of_html_code_index:])
 
 
 
