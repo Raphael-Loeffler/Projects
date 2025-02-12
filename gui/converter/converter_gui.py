aaaef2eba75e52074e6fb469cbde0864028a0c70
Raphael Loeffler <raphael.loeffler2010@gmail.com>
2025-02-12 22:02:52 +0100

    added features to converter_gui.py

--- gui/converter/converter_gui.py
+++ gui/converter/converter_gui.py
@@ -1,48 +1,82 @@
 import tkinter as tk
 from tkinter import ttk
 
+# TODO:
+# 1. Change the title of the application - Done
+# TODO! 2. In the same app, I want to be able to convert different quantities
+# 3. Make it not resizable (hint: in/after window.geometry) - Done
+# Extras:
+# 1. (entry of miles) mi = (label for kilometers) km
+# 2. The main window should appear in the middle of the screen. (for any screen)
+
+class ConverterPack:
+  def __init__(self, row, window, label, number_description, output_description, convert_type) -> None:
+    self.window = window
+    
+    self.row: int = row
+    self.label: str = label
+    self.number_description_text: str = number_description
+    self.output_description: str = output_description
+    self.convert_type: str = convert_type
+    
+    for i in range(row):
+      self.window.grid_rowconfigure(i, minsize=20)
+  
+  def run(self) -> None:
+    self.window.grid_rowconfigure(self.row, minsize=20)
+    self.window.grid_rowconfigure(self.row+1, minsize=20)
+    self.window.grid_rowconfigure(self.row+2, minsize=20)
+    
+    self.title_label = ttk.Label(master=self.window, text=self.label, font="Calibri 14 bold")
+    self.title_label.grid(row=self.row, column=0, sticky="w")
+    
+    self.entry_int = tk.IntVar()
+    self.entry_field = ttk.Entry(master=self.window, textvariable=self.entry_int, width=12)
+    self.entry_field.grid(row=self.row+1, column=0, sticky="w")
+    
+    self.number_description = ttk.Label(master=self.window, text=self.number_description_text, font="Calibri 12 bold")
+    self.number_description.grid(row=self.row+1, column=0, padx=(90, 0), sticky="w")
+    
+    self.convert_button = ttk.Button(text="Convert", command=self.convert)
+    self.convert_button.grid(row=self.row+1, column=0, padx=(164, 0), sticky="w")
+  
+  def convert(self) -> None:
+    self.output_str = tk.StringVar()
+    try:
+      match self.convert_type:
+        case "distance": self.output_str.set(f"{(self.entry_int.get() * 1.61):.02f}")
+        case "currency": self.output_str.set(f"{(self.entry_int.get() * 25.05):.02f}")
+        case "temperature": self.output_str.set(f"{(self.entry_int.get() * (9/5) + 32):.02f}")
+        case "weight": self.output_str.set(f"{(self.entry_int.get() * 0.4535924):.02f}")
+        case _: ...
+    except:
+      self.output_str.set("Something went wrong... Restart for proper use")
+      self.output_description = ""
+    
+    self.output = ttk.Label(master=self.window, text=f"{self.output_str.get()} {self.output_description}", font="Calibri 12 bold")
+    self.output.grid(row=self.row+2, column=0, padx=(0, 0), sticky="w")
+
 
 class ConverterGUIApp:
-  def run(self) -> None:
-    pass
+  def run(self):
+    window = tk.Tk()
+    window.title("Converter")
+    window.geometry("300x250")
+    window.resizable(False, False)
+    
+    distance_converter: ConverterPack = ConverterPack(0, window, "Distance Converter", "Miles", "Kilometers", "distance")
+    currency_converter: ConverterPack = ConverterPack(3, window, "Currency Converter", "Euro", "Czk", "currency")
+    temperature_converter: ConverterPack = ConverterPack(6, window, "Temperature Converter", "Celsius", "Fahrenheit", "temperature")
+    weight_converter: ConverterPack = ConverterPack(9, window, "Weight Converter", "Pound", "Kilograms", "weight")
+    
+    distance_converter.run()
+    currency_converter.run()
+    temperature_converter.run()
+    weight_converter.run()
+    
+    window.mainloop()
 
-window = tk.Tk()
-window.title("Distance Converter")
-window.geometry("300x150")
-
-def convert():
-  print(f"output_str={output_str.get()}")           #output_str=first output
-  print(f"entry_int={entry_int.get()}")             #entry_int=10
-  output_str.set(f"{entry_int.get() * 1.61} km")    #16.1 km
-  print(f"output_str={output_str.get()}")           #output_str=16.1 km
-
-# Constant text
-title_label = ttk.Label(master=window, text="Miles to kilometers", font="Calibri 14 bold")
-title_label.pack(pady=10)
-
-input_frame = ttk.Frame(master=window)
-input_frame.pack(pady=10)
-entry_int = tk.IntVar()
-entry_field = ttk.Entry(master=input_frame, textvariable=entry_int)
-entry_field.pack(side="left", padx=10)
-convert_button = ttk.Button(master=input_frame, text="Convert", command=convert)
-convert_button.pack(side="left")
-
-output_str = tk.StringVar()
-output_label = ttk.Label(master=window, text="", font="Calibri 10", textvariable=output_str)
-output_label.pack(pady=10)
-
-window.mainloop()
 
 if __name__ == "__main__":
   app = ConverterGUIApp()
   app.run()
-  print("Main")
-
-# TODO:
-# 1. Change the title of the application
-# 2. In the same app, I want to be able to convert different quantities
-# 3. Make it not resizable (hint: in/after window.geometry)
-# Extras:
-# 1. (entry of miles) mi = (label for kilometers) km
-# 2. The main window should appear in the middle of the screen. (for any screen)
